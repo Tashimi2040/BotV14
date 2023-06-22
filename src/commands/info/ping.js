6069d078cdf507b70ce2490866fa2841cd7ae381
Tashimi2040 <132114580+Tashimi2040@users.noreply.github.com>
2023-06-22 19:22:22 -0300

    Exemplos

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,14 +1,18 @@
+/*EXEMPLO DE COMANDO: esse e o de ping obvio mas serve como uma base*/
+
+
 const Command = require('../../structures/Command')
 
 module.exports = class extends Command {
     constructor(client) {
         super(client, {
-            name: 'ping',
-            description: 'Mostra o ping do bot.'
+            name: 'ping', //Nome do comando
+            description: 'Mostra o ping do bot.' //descrição
         })
     }
 
     run = (interaction) => {
+        /*Aqui vai o comando....*/
         interaction.reply({
             content: `O ping do bot é \`${this.client.ws.ping}\`ms.`,
         })
